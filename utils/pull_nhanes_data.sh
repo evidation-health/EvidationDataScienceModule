bd29efe6473620124dfa0a03933069784fb62b3e
bradshawben <bradshaw.ben@gmail.com>
2018-05-14 08:17:17 -0700

    Pull script moves file to home, update .hf store name

--- utils/pull_nhanes_data.sh
+++ utils/pull_nhanes_data.sh
@@ -3,8 +3,13 @@
 # Note: This script defaults to moving the pulled data into the user's home directory
 
 # Pull the .h5 NHANES datastore down from the public bucket on s3
+echo "Pulling down data from s3 bucket."
 wget --no-check-certificate https://s3.amazonaws.com/evi-ds-module/nhanes_data.tar.gz
+echo "Moving tarball to home directory."
 mv nhanes_data.tar.gz ~/.
 
 # Unzip the file
-tar -xvzf ~/nhanes_data.tar.gz
+echo "Unzipping tarball."
+tar -xvzf ~/nhanes_data.tar.gz ~/.
+
+echo "You are good to go!"
